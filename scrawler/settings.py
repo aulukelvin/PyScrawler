9a0155187d232e1a44b3a8a46d0dc92c858f418a
KelvinSTD <kelvinstd@Kelvins-MacBook-Pro.local>
2016-09-24 11:06:57 +1000

    unbar robot accessing to readability

--- scrawler/settings.py
+++ scrawler/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'scrawler.spiders'
 #USER_AGENT = 'scrawler (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
